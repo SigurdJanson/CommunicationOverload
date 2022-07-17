b3f008cf0677b0a560ea256fa232411516983e48
SigurdJanson <emailjs@web.de>
2022-07-17 16:49:15 +0200

    Documentation and various layout and content improvements

--- app.R
+++ app.R
@@ -67,36 +67,32 @@ ui <- fluidPage(
          column(3,
            h3("Team Relationships"),
            p("How many relationships between pairs of team members must be
-             maintained depending on the size of the team?
+             maintained depending on the size of the team? According to Brookes (1995)
+             sometimes called Brookes law.
              Adjust the slider to show this the effect."),
            sliderInput("inpTeamMembers",
                        "People on the Team",
                        min = min(.MemberRange),
                        max = max(.MemberRange),
-                       value = mean(.MemberRange)),
-           textOutput("comCount"),
-           textOutput("comRelative"),
+                       value = 8L),
+           textOutput("comCount")
          ),
          column(6,
            # Show a plot of the generated distribution
-           plotOutput("comPlot", height = "500px")
+           div(style=paste0("border: 1px solid ", .FGCol),
+            plotOutput("comPlot")
+           )
          ),
          column(3L,
-                p("The number of relationships to maintain in a team
-                  is sometimes called Brookes law (according to Brookes, 1995)"),
                 p("Please note: The link between team communication and
                 performance seems to be stronger when you focus on the
                 quality, rather than the quantity of communication."),
-                p("Reading"),
+                h6("Further Reading"),
                 FormatBib("Brooks, F. P.", "The Mythical Man Month", 1995, "Addison-Wesley"),
                 FormatBib("Marlow, S. L., Lacerenza, C. N., Paoletti, J., Burke, C. S., & Salas, E.",
                           "Does team communication represent a one-size-fits-all approach?: A meta-analysis of team communication and performance",
                           "2018", journal="Organizational Behavior and Human Decision Processes",
-                          issue=144, pg="145-170"),
-                p("Marlow, S. L., Lacerenza, C. N., Paoletti, J., Burke, C. S., & Salas, E. (2018).
-                  'Does team communication represent a one-size-fits-all approach?: A meta-analysis
-                  of team communication and performance'.
-                  Organizational Behavior and Human Decision Processes, 144, 145-170."),
+                          issue=144, pg="145-170")
          )
   )),
 
@@ -112,19 +108,23 @@ ui <- fluidPage(
                             min = 0, max = 5, step = 0.1,
                             value = 1, post = "%")),
          column(6L,
-                plotOutput("outProductiveTime", height = "500px")
+                div(style=paste0("border: 1px solid ", .FGCol),
+                  plotOutput("outProductiveTime")
+                )
          ),
-         column(3L,
-                p(tags$sup("🕂", .noWS = "outside"), "Such a reduction could be because of ...",
-                  tags$ul(
-                    tags$li("an increased difficulty to develop quality relationships,"),
-                    tags$li("unrelated communcation that does not contribute to performance,"),
-                    tags$li("the ", a("Ringelmann Effect",
-                                 href = "https://en.wikipedia.org/w/index.php?title=Ringelmann_effect&oldid=1067453356"))
+         column(3L, withTags(
+                p(sup("🕂", .noWS = "outside"), small("Such a reduction could be caused by ...",
+                  ul(
+                    li("an increased difficulty to develop quality relationships,"),
+                    li("unrelated communcation that does not contribute to performance,"),
+                    li("the ", a("Ringelmann Effect",
+                                  href = "https://en.wikipedia.org/w/index.php?title=Ringelmann_effect&oldid=1067453356"))
                   )
-                ),
-                p("The assumption in this simulation is a linear relationship between the number
-                  of team members and the loss in productivity.")
+                ))),
+                withTags(
+                  p(small("The assumption in this simulation is a linear relationship between the number
+                    of team members and the loss in productivity."))
+                )
          )
     )
   )
@@ -136,17 +136,16 @@ server <- function(input, output) {
 
 
   output$comCount <- renderText({
-    Members <- (input$inpTeamMembers-1):(input$inpTeamMembers+1)
+    Members <- input$inpTeamMembers
     Paths <- PathsOfTalk(Members)
-    paste("Count:", paste(Paths, collapse = " / "))
+    paste("Relationships:", paste(Paths, collapse = " / "))
   })
 
-  output$comRelative <- renderText({
-    Members <- (input$inpTeamMembers-1):(input$inpTeamMembers+1)
-    Paths <- PathsOfTalk(Members)
-    paste("Relation:", paste(Paths / Members, collapse = " / "))
-  })
 
+
+  #
+  # Plot Communication Pathways
+  #
   output$comPlot <- renderPlot({
     nodes <- 1:input$inpTeamMembers
     edges <- combn(nodes, 2)
@@ -155,25 +154,29 @@ server <- function(input, output) {
     graph <- make_graph( edges=edges, directed=FALSE ) %>%
       set_vertex_attr("label", value = rep("", input$inpTeamMembers))
     layout <- layout_in_circle(graph, order = V(graph))
+    par.backup <- par(mar=c(0,0,0,0)+.1)
 
     ## PLOT
     plot(graph, layout=layout,
          vertex.size = 10, color = .PrimaryCol,
-         vertex.frame.width = 1,
-         vertex.shape = "circle", frame = TRUE)
+         vertex.frame.width = 0,
+         vertex.shape = "circle", frame = FALSE)
+    par(par.backup)
   })
 
 
-
+  #
+  # Plot for Productivity
+  #
   output$outProductiveTime <- renderPlot({
     MemberCount <- min(.MemberRange):max(.MemberRange)
     Waste <- input$inpCommunctionWaste / 100
     ProductiveTime <- ProductiveTimeLeft(MemberCount, Waste)
 
-    ggplot(data.frame(Members = MemberCount, ProductiveTime = ProductiveTime),
+    ggplot(data.frame(Members = MemberCount, ProductiveTime = ProductiveTime*100),
            aes(x = Members, y = ProductiveTime, label = format(ProductiveTime))) +
       geom_line() + geom_point() +
-      ggrepel::geom_text_repel() +
+      #ggrepel::geom_text_repel() +
       geom_vline(xintercept = input$inpTeamMembers, color = .PrimaryCol) +
       theme(axis.text.y   = element_text(size=12),
             axis.text.x   = element_text(size=12),
@@ -182,12 +185,15 @@ server <- function(input, output) {
             panel.background = element_blank(),
             panel.grid.major = element_blank(),
             panel.grid.minor = element_blank(),
-            axis.line = element_line(colour=.FGCol),
-            panel.border = element_rect(colour=.FGCol, fill=NA, size=1))
+            panel.border = element_rect(colour=NA, fill=NA, size=1),
+            axis.line = element_line(colour=.FGCol))
   })
+
 }
 
 
 
+#
 # Run the application
+#
 shinyApp(ui = ui, server = server)
