e05b3db2215a7b2d045e31255894eda0ff9f2666
SigurdJanson <emailjs@web.de>
2022-07-17 12:54:27 +0200

    Fixed reference formatting

--- helpers.R
+++ helpers.R
@@ -1,24 +1,28 @@
 
-
 FormatBib <- function(aut, title, year,
                       publ=NULL, journal=NULL, issue=NULL, nr=NULL, pg=NULL) {
-  .a <- function(x) if (missing(x)) return(NULL) else return(x)
-  .add <- function(pre, x, post) { if(is.null(x)) return(NULL); paste0(.a(pre), x, .a(post)) }
-  allWS <- c("before", "after", "after-begin", "before-end", "outside", "inside")
 
   stopifnot(!missing(aut))
   stopifnot(!missing(title))
   stopifnot(!missing(year))
 
-  p(aut,
-    .add("(", year, ")."),
-    "'", tag("em", title, .noWS = "outside"), "'",
-    .add(". ", publ),
-    .add(". ", journal),
-    .add(", ", issue),
-    .add(" (", nr, ")"),
-    .add(", ", pg),
-    .noWS = "inside"
+  .a <- function(x) if (missing(x)) return(NULL) else return(x)
+  .add <- function(pre, x, post) { if(is.null(x)) return(NULL); paste0(.a(pre), x, .a(post)) }
+  allWS <- c("before", "after", "after-begin", "before-end", "outside", "inside")
+
+  HTML(
+    paste0(
+      "<p>",
+        aut, " (", year, "). ",   # author (year)
+        "'<em>", title, "</em>'", # title
+        paste0(
+          .add(". ", publ),
+          .add(". ", journal),
+          .add(", ", issue),
+          .add(" (", nr, ")"),
+          .add(", ", pg)
+        ),
+      "</p>"),
+    .noWS = allWS
   )
 }
-# p("Brooks, F. P. (1995).", tags$em("'The Mythical Man Month'"), ". Addison-Wesley"),
